a69850f05416072bec190d8b2e87f7384701a2db
bwiedower1 <b.wiedower!@gmail.com>
2025-08-03 00:16:22 -0500

    header pt 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [
+    RouterOutlet,
+    ButtonModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class App {
   protected readonly title = signal('project-hail-mary');
